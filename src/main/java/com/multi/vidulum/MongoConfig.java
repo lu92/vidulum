995d5fcad2802d1a90d6b2959f96158e69eb7727
lu92 <biklucjan@gmail.com>
2021-03-29 01:05:59 +0200

    VID-3: set connection with mongo db

--- src/main/java/com/multi/vidulum/MongoConfig.java
+++ src/main/java/com/multi/vidulum/MongoConfig.java
@@ -4,6 +4,7 @@ import com.mongodb.ConnectionString;
 import com.mongodb.MongoClientSettings;
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.core.MongoTemplate;
@@ -11,9 +12,19 @@ import org.springframework.data.mongodb.core.MongoTemplate;
 @Configuration
 public class MongoConfig {
 
+    @Value("${mongodb.host}")
+    private String host;
+
+    @Value("${mongodb.port}")
+    private String port;
+
+    @Value("${mongodb.database}")
+    private String database;
+
+
     @Bean
     public MongoClient mongo() {
-        ConnectionString connectionString = new ConnectionString("mongodb://localhost:27017/test1");
+        ConnectionString connectionString = new ConnectionString(String.format("mongodb://%s:%s/%s", host, port, database));
         MongoClientSettings mongoClientSettings = MongoClientSettings.builder()
                 .applyConnectionString(connectionString)
                 .build();
@@ -22,7 +33,7 @@ public class MongoConfig {
     }
 
     @Bean
-    public MongoTemplate mongoTemplate() throws Exception {
-        return new MongoTemplate(mongo(), "test1");
+    public MongoTemplate mongoTemplate() {
+        return new MongoTemplate(mongo(), database);
     }
 }
