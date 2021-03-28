9dcdb0b09f5add265b753b072637860bb9dd9eec
lu92 <biklucjan@gmail.com>
2021-03-29 00:54:37 +0200

    VID-3: set connection with mongo db

--- src/main/java/com/multi/vidulum/VidulumApplication.java
+++ src/main/java/com/multi/vidulum/VidulumApplication.java
@@ -1,13 +1,28 @@
 package com.multi.vidulum;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
-public class VidulumApplication {
+@EnableMongoRepositories(basePackages = "com.multi.vidulum")
+public class VidulumApplication implements CommandLineRunner {
 
-	public static void main(String[] args) {
-		SpringApplication.run(VidulumApplication.class, args);
-	}
+    @Autowired
+    private MongoTemplate mongoTemplate;
 
+    public static void main(String[] args) {
+        SpringApplication.run(VidulumApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        mongoTemplate.dropCollection(Sample.class);
+        Sample hello_world = Sample.builder().text("Hello world").build();
+        Sample inserted = mongoTemplate.insert(hello_world);
+        System.out.println(inserted);
+    }
 }
