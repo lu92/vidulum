45852f633d66c8666d24bdd8ebe33556c83b0420
lu92 <biklucjan@gmail.com>
2022-01-11 16:27:47 +0100

    implementation

--- src/main/java/com/multi/vidulum/portfolio/app/listeners/OrderCreatedEventListener.java
+++ src/main/java/com/multi/vidulum/portfolio/app/listeners/OrderCreatedEventListener.java
@@ -1,5 +1,8 @@
 package com.multi.vidulum.portfolio.app.listeners;
 
+import com.multi.vidulum.common.Money;
+import com.multi.vidulum.common.OrderType;
+import com.multi.vidulum.common.Quantity;
 import com.multi.vidulum.common.Side;
 import com.multi.vidulum.common.events.OrderCreatedEvent;
 import com.multi.vidulum.portfolio.app.commands.lock.LockAssetCommand;
@@ -23,14 +26,18 @@ public class OrderCreatedEventListener {
     public void on(OrderCreatedEvent event) {
         log.info("OrderCreatedEvent [{}] has been captured", event);
 
-        if (Side.SELL.equals(event.getSide())) {
-            lockAssetQuantity(event);
-        } else {
+        if (isOrderWithPurchaseAttempt(event)) {
             lockBalanceForPurchaseOfAsset(event);
+        } else {
+            lockAssetForSale(event);
         }
     }
 
-    private void lockAssetQuantity(OrderCreatedEvent event) {
+    private boolean isOrderWithPurchaseAttempt(OrderCreatedEvent event) {
+        return Side.BUY.equals(event.getSide());
+    }
+
+    private void lockAssetForSale(OrderCreatedEvent event) {
         LockAssetCommand command = LockAssetCommand.builder()
                 .portfolioId(event.getPortfolioId())
                 .ticker(event.getSymbol().getOrigin())
@@ -40,10 +47,15 @@ public class OrderCreatedEventListener {
     }
 
     private void lockBalanceForPurchaseOfAsset(OrderCreatedEvent event) {
+        Money money = OrderType.OCO.equals(event.getType()) ?
+                event.getTargetPrice().multiply(event.getQuantity()) :
+                event.getLimitPrice().multiply(event.getQuantity());
+        Quantity quantityToLocked = Quantity.of(money.getAmount().doubleValue());
+
         LockAssetCommand command = LockAssetCommand.builder()
                 .portfolioId(event.getPortfolioId())
                 .ticker(event.getSymbol().getDestination())
-                .quantity(event.getQuantity())
+                .quantity(quantityToLocked)
                 .build();
         commandGateway.send(command);
     }
