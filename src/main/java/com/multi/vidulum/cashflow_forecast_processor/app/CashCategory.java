9cbcf13f96cc331820b7e30897c3bac070539719
lu92 <biklucjan@gmail.com>
2024-02-12 20:01:27 +0100

    VID-53: Improve CashFlowForecastProcessor to adopting particular events

--- src/main/java/com/multi/vidulum/cashflow_forecast_processor/app/CashCategory.java
+++ src/main/java/com/multi/vidulum/cashflow_forecast_processor/app/CashCategory.java
@@ -11,22 +11,6 @@ import java.util.List;
 public class CashCategory {
     private Category category;
     private List<CashCategory> subCategories;
-//    private Map<PaymentStatus, List<TransactionDetails>> transactions;
     private GroupedTransactions groupedTransactions;
     private Money totalValue;
-
-//    public Transaction findTransaction(CashChangeId cashChangeId) {
-//        return transactions.entrySet().stream()
-//                .map(paymentTransactions -> paymentTransactions.getValue().stream()
-//                        .filter(transactionDetails -> cashChangeId.equals(transactionDetails.getCashChangeId()))
-//                        .findFirst()
-//                        .map(transactionDetails -> Map.entry(paymentTransactions.getKey(), transactionDetails)))
-//                .filter(Optional::isPresent)
-//                .findFirst()
-//                .filter(Optional::isPresent)
-//                .map(Optional::get)
-//                .map(paymentStatusTransactionDetailsEntry -> new Transaction(paymentStatusTransactionDetailsEntry.getValue(), paymentStatusTransactionDetailsEntry.getKey()))
-//                .orElseThrow(() -> new IllegalStateException(
-//                        String.format("Cannot find transaction for CashChange [%s]", cashChangeId)));
-//    }
 }
