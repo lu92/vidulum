953eae396941e327737fe1da1bf2e9ba9b289378
lu92 <biklucjan@gmail.com>
2024-07-28 01:01:09 +0200

    VID-73: Security for application

--- src/main/java/com/multi/vidulum/security/demo/DemoController.java
+++ src/main/java/com/multi/vidulum/security/demo/DemoController.java
@@ -1,6 +1,5 @@
 package com.multi.vidulum.security.demo;
 
-import io.swagger.v3.oas.annotations.Hidden;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,12 +7,11 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1/demo-controller")
-@Hidden
 public class DemoController {
 
-  @GetMapping
-  public ResponseEntity<String> sayHello() {
-    return ResponseEntity.ok("Hello from secured endpoint");
-  }
+    @GetMapping
+    public ResponseEntity<String> sayHello() {
+        return ResponseEntity.ok("Hello from secured endpoint");
+    }
 
 }
